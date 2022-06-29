61e00b49419fcc8f67224fcd49a6901b6491a0bc
Andrea Pistelli <apisti01@gmail.com>
2022-06-29 17:21:25 +0200

    added sfml libraries and basic main to check if it works

---
+++ GameCharacter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by apisti01 on 29/06/22.
+//
+
+#include "GameCharacter.h"
---
+++ GameCharacter.h
@@ -0,0 +1,14 @@
+//
+// Created by apisti01 on 29/06/22.
+//
+
+#ifndef MAIN_CPP_GAMECHARACTER_H
+#define MAIN_CPP_GAMECHARACTER_H
+
+
+class GameCharacter {
+
+};
+
+
+#endif //MAIN_CPP_GAMECHARACTER_H
